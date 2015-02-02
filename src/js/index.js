c508f918be43a89d837b7530b81214d3ed43b0b9
MaxSharp <charpmax@gmail.com>
2015-02-02 11:25:27 +0100

    demo v1 finished -- test kinect OK

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
+
+//// KINECT-SETUP ////
 var kinect = EkWinjs.Kinect.getInstance();
 
-
-
 var body = kinect.bodyFrame.trackBodyOn(0.5, 0.2, 1.5, 0.2);
-
 
 kinect.addCanvasDebug("mainCanvas");
 
 kinect.addCanvasDebug("resultCanvas", true);
 
 
+//// GRID-LISTENERS ////
 var tiles = document.getElementsByClassName("element");
 
 for (var i = tiles.length - 1; i >= 0; i--) {
@@ -20,19 +20,37 @@ for (var i = tiles.length - 1; i >= 0; i--) {
   body.pointer.addEventListener(EkWinjs.Kinect.Events.Pointer.HOLD_END, functionHoldEnd,tiles[i],{handClosed:true});
 };
 
-body.pointer.addEventListener(EkWinjs.Kinect.Events.Pointer.MOVE, functionMoveHandler, document.body);
+  body.pointer.addEventListener(EkWinjs.Kinect.Events.Pointer.MOVE, functionMoveHandler, document.body);
 
+
+//// GRID-EVENTS-FUNCTIONS ////
 function functionMoveHandler(target) {
-    console.log(body.pointer.x);
+
+    
+    posX += (body.pointer.x - posX) * 0.5;
+    posY += (body.pointer.y - posY) * 0.5;
+
+    $('.loader').css({
+       left:  posX - 25,
+       top:   posY - 25 
+    });
+
+    $('.cursor').css({
+       left:  posX - 10,
+       top:   posY - 10,
+       opacity: 1 
+    });
 };
 
 function functionHoldStart(target) {
+
     ekolor = ekolors[Math.floor(Math.random() * ekolors.length)];
     target.childNodes[0].style.background = ekolor;
     target.style.background = "black";
 };
 
 function functionHoldProgress(target,progress) {
+
   $('.loader').css('opacity', '1');
   target.childNodes[0].style.width = (progress * 100) + "%";
   target.childNodes[0].style.height = (progress * 100) + "%";
@@ -41,19 +59,24 @@ function functionHoldProgress(target,progress) {
   target.childNodes[0].style.opacity = progress;
   $('.loader--inpt').val((progress * 100)).trigger('change');
 
-  if (progress >= 1) {
-    //$('.loader').css('opacity', '0');
+  if ((progress * 100) < 100) {
+    setTimeout(function(){ 
+      $('.loader').css('opacity', '0');
+    }, 750);
   }
 };
 
 function functionHoldEnd(target) {
+
+  $('.loader').css('opacity', '0');
   target.style.opacity = 1;
-  removeClass(target, "square");
-  addClass(target, "circle");
+  $(target.className).removeClass("square");
+  $(target.className).addClass("circle");
   target.style.background = ekolor;
 };
 
 function functionOver(target) {
+
   if (hasClass(target,"square")) {
    target.style.borderRadius = "50%";
   } else if (hasClass(target,"circle")) {
@@ -62,18 +85,72 @@ function functionOver(target) {
 };
 
 function functionOut(target) {
+
+  $('.loader').css('opacity', '0');
   if (hasClass(target,"square")) {
    target.style.borderRadius = "0%";
   } else if (hasClass(target,"circle")) {
    target.style.borderRadius = "50%";
   }
+};
+
+
+//// BTN-LISTENERS ////
+var confBtn = document.getElementById("configBtn");
+var confBrd = document.getElementById("configBoard");
+
+body.pointer.addEventListener(EkWinjs.Kinect.Events.Pointer.OVER, functionOverBtn,confBtn);
+body.pointer.addEventListener(EkWinjs.Kinect.Events.Pointer.OUT, functionOutBtn,confBtn);
+body.pointer.addEventListener(EkWinjs.Kinect.Events.Pointer.HOLD_START, functionHoldStartBtn,confBtn,{handClosed:true});
+body.pointer.addEventListener(EkWinjs.Kinect.Events.Pointer.HOLD_PROGRESS, functionHoldProgressBtn,confBtn,{handClosed:true});
+body.pointer.addEventListener(EkWinjs.Kinect.Events.Pointer.HOLD_END, functionHoldEndBtn,confBtn,{handClosed:true});
+
+
+//// BTN-EVENTS-FUNCTIONS ////
+function functionOverBtn(target) {};
+
+function functionOutBtn(target) {
+  $('.loader').css('opacity', '0');
+};
+
+function functionHoldStartBtn(target) {};
+
+function functionHoldProgressBtn(target, progress) {
+    $('.loader').css('opacity', '1');
+    $('.loader--inpt').val((progress * 200)).trigger('change');
+
+    if ((progress * 100) == 50) {
+      var boardDsply = $("#configBoard").css('display');
+      
+      if (boardDsply == 'none') {
+        $("#configBoard").css('display', 'block');
+      } else if (boardDsply == 'block') {
+        $("#configBoard").css('display', 'none');
+      }
+    };
+
+    if ((progress * 100) < 50) {
+    setTimeout(function(){ 
+      $('.loader').css('opacity', '0');
+    }, 750);
+  }
+};
+
+function functionHoldEndBtn(target) {
+    
+    body.pointer.addEventListener(EkWinjs.Kinect.Events.Pointer.HOLD_START, functionHoldStartBtn,confBtn,{handClosed:true});
+    body.pointer.addEventListener(EkWinjs.Kinect.Events.Pointer.HOLD_PROGRESS, functionHoldProgressBtn,confBtn,{handClosed:true});
+    body.pointer.addEventListener(EkWinjs.Kinect.Events.Pointer.HOLD_END, functionHoldEndBtn,confBtn,{handClosed:true});
 };  
 
 
 /////////////////////////////////////////
-/////////////////////////////////////////
+///////////////-TOOLS-///////////////////
 /////////////////////////////////////////
 
+function hasClass(ele,cls) {
+  return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+}
 
 var ekolors = [
   "#A9A8A9",
@@ -87,26 +164,11 @@ var ekolors = [
   "#DC0031"
 ]
 
-function hasClass(ele,cls) {
-  return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
-}
-
-function addClass(ele,cls) {
-  if (!hasClass(ele,cls)) ele.className += " "+cls;
-}
-
-function removeClass(ele,cls) {
-  if (hasClass(ele,cls)) {
-    var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
-    ele.className=ele.className.replace(reg,' ');
-  }
-}
-
 $(".loader--inpt").knob({
   'min':0,
   'max':100,
   'width':50,
   'thickness':0.2,
-  'fgColor':"#FF4611",
+  'fgColor':"#FFB718",
   'displayInput':'false'
 });
